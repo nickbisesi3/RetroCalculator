2c0e5155876cb593f36670ca796586a29536e681
nickbisesi3 <nickbisesi3@gmail.com>
2016-11-27 16:11:42 -0500

    Started project
    got rid of memory warning @ bottom of VC

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Nick Bisesi on 11/27/16.
+//  Copyright © 2016 Nick Bisesi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
